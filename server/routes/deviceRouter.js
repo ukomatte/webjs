a48e7e6fcde2446fcb8b740333ddd81c21b7a665
ukomatte <ukomatte@gmail.com>
2022-02-05 01:25:30 +0300

    F

--- server/routes/deviceRouter.js
+++ server/routes/deviceRouter.js
@@ -1,9 +1,11 @@
 const Router = require('express')
 const router = new Router()
+const deviceController = require('../controllers/deviceController')
 
-router.post('/',)
-router.get('/',)
-router.get('/:ID',)
+
+router.post('/', deviceController.create)
+router.get('/', deviceController.getAll)
+router.get('/:ID', deviceController.getAll )
 
 
 module.exports = router
